8df68e1fcb881328208e07ad1a547b8e212d3a83
Thirki <henriquegomesdev@outlook.com>
2024-05-24 15:48:24 -0300

    Feat: clear theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { Theme } from "./styles/Theme.tsx";
+import { GlobalStyle } from './styles/global.ts';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <GlobalStyle />
+      <App />
+    </Theme>
   </React.StrictMode>,
 )
